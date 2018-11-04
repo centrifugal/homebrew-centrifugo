795ad4855c98a1b2aee4199d04417941fb5955ed
Alexander Emelin <frvzmb@gmail.com>
2018-11-04 21:04:37 +0300

    Brew formula update for centrifugo version v2.0.2

--- Formula/centrifugo.rb
+++ Formula/centrifugo.rb
@@ -1,9 +1,9 @@
 class Centrifugo < Formula
   desc "Real-time messaging server"
   homepage "https://github.com/centrifugal/centrifugo"
-  url "https://github.com/centrifugal/centrifugo/releases/download/v2.0.1/centrifugo_2.0.1_darwin_amd64.tar.gz"
-  version "2.0.1"
-  sha256 "8d742e4246914a4d3072f989db2652dd8c7f8e9791cd13fbeea952e6824e80d7"
+  url "https://github.com/centrifugal/centrifugo/releases/download/v2.0.2/centrifugo_2.0.2_darwin_amd64.tar.gz"
+  version "2.0.2"
+  sha256 "82cbb1676c4d3c897426f83c39664b714601227b01e2d0c4cffa1bd687a9d11a"
 
   def install
     bin.install "centrifugo"
