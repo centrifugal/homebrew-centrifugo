a9811354efbde3d2af6a36553046aa8b6bf666e8
Alexander Emelin <frvzmb@gmail.com>
2021-05-24 09:44:55 +0300

    Brew formula update for centrifugo version v2.8.5

--- Formula/centrifugo.rb
+++ Formula/centrifugo.rb
@@ -5,28 +5,28 @@
 class Centrifugo < Formula
   desc "Real-time messaging server"
   homepage "https://github.com/centrifugal/centrifugo"
-  version "2.8.4"
+  version "2.8.5"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/centrifugal/centrifugo/releases/download/v2.8.4/centrifugo_2.8.4_darwin_amd64.tar.gz"
-    sha256 "2dbc3408d86a076a004e11b485153b81523b06e460926850bd4871bb3bc239a3"
+    url "https://github.com/centrifugal/centrifugo/releases/download/v2.8.5/centrifugo_2.8.5_darwin_amd64.tar.gz"
+    sha256 "456d6cc54ab2a1d395d9458081a3981411cd0e42dfef54e3d4c8c5fc74770686"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/centrifugal/centrifugo/releases/download/v2.8.4/centrifugo_2.8.4_darwin_arm64.tar.gz"
-    sha256 "04131f011b5403a31bdcf764a1be7986f1365d7268e6fd33678835de190cf684"
+    url "https://github.com/centrifugal/centrifugo/releases/download/v2.8.5/centrifugo_2.8.5_darwin_arm64.tar.gz"
+    sha256 "d0b22197e0fda6acbbbbe6870d9a75986f7ef4663824baf6e98a0abc860fe161"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/centrifugal/centrifugo/releases/download/v2.8.4/centrifugo_2.8.4_linux_amd64.tar.gz"
-    sha256 "bbb23b82fb4a926a802328d22718248b1ae7852ad6504b332056d40550eb1a51"
+    url "https://github.com/centrifugal/centrifugo/releases/download/v2.8.5/centrifugo_2.8.5_linux_amd64.tar.gz"
+    sha256 "3482a7388ede53d3a9ae9769a2a2b9ecee701073aa250ea0b74370a6d1fc60bb"
   end
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://github.com/centrifugal/centrifugo/releases/download/v2.8.4/centrifugo_2.8.4_linux_armv6.tar.gz"
-    sha256 "d011a5a57dacc8780f74548d80ff18e9c46aea779455619cbd5e5712956b9a4e"
+    url "https://github.com/centrifugal/centrifugo/releases/download/v2.8.5/centrifugo_2.8.5_linux_armv6.tar.gz"
+    sha256 "204fac57d4964104c987bbb86676c1bf6a7b7b61b191d6bd7ae7d9e37214925d"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/centrifugal/centrifugo/releases/download/v2.8.4/centrifugo_2.8.4_linux_arm64.tar.gz"
-    sha256 "fb7bba6f1e824ba93ce77618aff3f7866c3c1d91892d31058a9620d7018174b0"
+    url "https://github.com/centrifugal/centrifugo/releases/download/v2.8.5/centrifugo_2.8.5_linux_arm64.tar.gz"
+    sha256 "aa96decd861a80c6aeb1f4d5e63531f4c68078ddd47a22ea8a3c2c87d10558e9"
   end
 
   def install
